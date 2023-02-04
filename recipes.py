f1bff4ac6e77c36f070fb6f9a4ca7a60b14497b1
lpinn <lep86@pitt.edu>
2023-02-03 21:08:32 -0500

    barebones py and js file

---
+++ recipes.py
@@ -0,0 +1,10 @@
+#heres the route handling 
+from flask import Flask, render_template, url_for, request, redirect
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+// Global Variables 
+var meals;
+window.addEventListener('load', setup); 
+
+function setup() {
+    
+}
+
+function displayMealInfo() {
+    let url = "www.themealdb.com/api/json/v1/1/random.php"
+}
+
