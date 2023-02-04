d80df4cc8bb67353a1badd3072644651fdef3f37
lpinn <lep86@pitt.edu>
2023-02-04 14:42:51 -0500

    working on the ingredient search

--- recipes.py
+++ recipes.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("")
-
+    return render_template("main-page.html")
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -3,10 +3,35 @@ var meals;
 window.addEventListener('load', setup); 
 
 function setup() {
-    
+    randomMeal = document.getElementById("meal");
+    randomMeal.addEventListener("click", displayRandomMealInfo);
+    ingMeal = document.getElementById("ingmeal");
+    ingMeal.addEventListener("click", getMeal);
 }
 
-function displayMealInfo() {
-    let url = "www.themealdb.com/api/json/v1/1/random.php"
+function displayRandomMealInfo() {
+    let url = "https://www.themealdb.com/api/json/v1/1/random.php"
+    fetch(url).then(res => res.json()).then(mealData => {
+            data = mealData.meals[0]
+            document.getElementById("meal-name").innerHTML = data.strMeal
+    })
 }
 
+function getMeal() {
+    let userInput = document.getElementById('usermealinput').value 
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${userInput}`).then(res => res.json())
+    .then(mealData => {
+        mealArray = mealData.meals[0]
+        alert(mealArray)
+        $(document).ready(function () {
+            $('#meals').DataTable({
+                data: mealArray,
+                columns: [
+                    { title: 'strMeal' },
+                    { title: 'strMealThumb' },
+                    { title: 'idMeal' },
+                ],
+            });
+        });
+    })
+}
